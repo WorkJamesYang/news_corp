069ad71ff5582714d85b0a3849364e426709314b
James <work.jamesyang@gmail.com>
2023-06-09 06:00:53 +1000

    add test and optimize code

--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -3,11 +3,16 @@ import React from 'react'
 export default function Article(props) {
     return (
         <li className="article-item">
-            <h4>{props.headline.default}</h4>
-            <span>{props.date.live}</span>
-            <div dangerouslySetInnerHTML={{__html: props.standfirst.default}}/>
-            {/* <img alt="article thumbnail" src={props.headline.default}/> */}
-            <a href={props.link.canonical}>Read more</a>
+            <div>
+                <img alt="article thumbnail" src={props.references[props.related.thumbnail.default[0]].link.media}/>
+            </div>
+            <div>
+                <h4>{props.headline.default}</h4>
+                <span>{props.date.live}</span>
+                <div dangerouslySetInnerHTML={{__html: props.standfirst.default}}/>
+                <a href={props.link.canonical}>Read more</a>
+            </div>
+            
         </li>
     )
 }
--- src/components/Pager/Pager.js
+++ src/components/Pager/Pager.js
@@ -1,10 +1,14 @@
 import React from 'react'
-import {getPageNumber, getMinNumber, getMaxNumber, toPage }from './utilities'
+import { getPageNumber, getMinNumber, getMaxNumber, toPage }from './utilities'
 import './Pager.css'
+
 /** 
  * pagination component
  */
 const Pager = props =>{
+    const {
+        currentPage,
+    } = props
     const pageNumber = getPageNumber(props)
     if(pageNumber === 0){
         return null
@@ -13,19 +17,19 @@ const Pager = props =>{
     const max = getMaxNumber(min, pageNumber, props)
     const numbers = []
     for(let i = min ; i <= max ; i ++){
-        numbers.push(<span key={i} onClick={() =>{toPage(i,props)}}  className={i === props.currentPage ? 'item active': 'item'}>{i}</span>)
+        numbers.push(<span key={i} onClick={() =>{toPage(i,props)}}  className={i === currentPage ? 'item active': 'item'}>{i}</span>)
     }
 
     return (
         <div className='pager'>
-            <span onClick={() =>{toPage(1,props)}} 
-                className={props.currentPage === 1 ? 'item disabled' : 'item'} >first</span>
-            <span  onClick={() =>{toPage(props.currentPage - 1 < 1 ? 1: props.currentPage -1,props)}}
-            className={props.currentPage === 1 ? 'item disabled' : 'item'}>prev</span>
+            <span data-testid="test-first" onClick={() =>{toPage(1, props)}} 
+                className={currentPage === 1 ? 'item disabled' : 'item'} >first</span>
+            <span  onClick={() =>{toPage(currentPage - 1 < 1 ? 1: currentPage -1, props)}}
+            className={currentPage === 1 ? 'item disabled' : 'item'}>prev</span>
             {numbers}
-            <span onClick={() =>{toPage(props.currentPage + 1 >  pageNumber ? pageNumber : props.currentPage + 1,props)}} className={props.currentPage === pageNumber ? 'item disabled' : 'item'}>next</span>
-            <span onClick={() =>{toPage(pageNumber,props)}} className={props.currentPage === pageNumber ? 'item disabled' : 'item'}>last</span>
-            <span className='current'>{props.currentPage}</span>/<span>{pageNumber}</span>
+            <span onClick={() =>{toPage(currentPage + 1 >  pageNumber ? pageNumber : currentPage + 1, props)}} className={currentPage === pageNumber ? 'item disabled' : 'item'}>next</span>
+            <span onClick={() =>{toPage(pageNumber, props)}} className={currentPage === pageNumber ? 'item disabled' : 'item'}>last</span>
+            <span className='current'>{currentPage}</span>/<span>{pageNumber}</span>
         </div>
     )
 }
