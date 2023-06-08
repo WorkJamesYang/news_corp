427744979f94008e9c450e50c781bf12c301dfd1
James <work.jamesyang@gmail.com>
2023-06-09 01:14:34 +1000

    slice article arrays by pagination

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
-import React, {useState} from 'react'
-import Article from './components/Article/Article'
+import React, {useState,useMemo} from 'react'
 import Pager from './components/Pager/Pager'
+import Article from './components/Article/Article'
 import data from './data/capi.json'
 import './App.css'
 
+let pageSize = 3
 function App() {
   const [currentPage, setCurrentPage] = useState(1)
+
+  /*get currentTableData */
+  const currentTableData = useMemo(() => {
+    const firstPageIndex = (currentPage - 1) * pageSize
+    const lastPageIndex = firstPageIndex + pageSize
+    return data.results.slice(firstPageIndex, lastPageIndex)
+  }, [currentPage])
+
   return (
     <>
       <ul>
-        {/* {data.results.map(item => <Article key={item.id} {...item}/>)} */}
+        {currentTableData.map(item => <Article key={item.id} {...item}/>)}
       </ul>
       <Pager
       articleLength={data.results.length}
       panelNumber={5}
       currentPage={currentPage}
-      pageSize={3}
+      pageSize={pageSize}
       onPageChange={page => setCurrentPage(page)}/>
     </>
   )
--- src/components/Pager/Pager.js
+++ src/components/Pager/Pager.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {getPageNumber, getMinNumber, getMaxNumber, toPage }from './utilities'
 import './Pager.css'
 /** 
  * pagination component
@@ -29,44 +30,3 @@ const Pager = props =>{
     )
 }
 export default Pager
-/**
- * 
- * calculate min panel number 
- */
-function getMinNumber(props) {
-    console.log(props)
-    let min = props.currentPage - Math.floor(props.panelNumber / 2 )
-    return min < 1 ? 1 : min
-}
-
-/**
- * 
- * @param {*} min 
- * @param {*} pageNumber 
- */
-function getMaxNumber(min, pageNumber, props){
-    let max = min  + props.panelNumber - 1
-    return max > pageNumber ? pageNumber : max
-
-}
-
-/**
- * navigate to certain page
- * @param {*} target target page number
- * @param {*} props  all props
- */
-function toPage(target, props){
-    if(props.currentPage === target){
-        return // Object.is('current page number','target page number')
-    }
-    props.onPageChange && props.onPageChange(target)
-}
-
-
-/**
- * calculate total number of pages
- * @param {*} props 
- */
-function getPageNumber(props){
-    return Math.ceil(props.articleLength / props.pageSize)
-}
